7d55dafe89e2bca242402edf022d97c5846fca46
Alejandro <alejohola24@gmail.com>
2024-11-11 15:50:21 -0500

    initial develop

--- obj/Debug/net8.0/PropertyManagerWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/PropertyManagerWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b644b45e64dd367db086241491b97a37e8f9f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
