399e2b5dafb6f60f5e3348def39dfdb90918cc59
Alejandro <alejohola24@gmail.com>
2024-11-21 16:52:21 -0500

    Edit propiedades

--- obj/Debug/net8.0/PropertyManagerWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/PropertyManagerWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+341179340ea353239504ca4baf311c103fa65aac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b218c083b769832c4780d7dbbe22264c15ab02f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
