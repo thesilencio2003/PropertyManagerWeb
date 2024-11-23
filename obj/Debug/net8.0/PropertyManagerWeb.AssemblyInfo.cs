8b4f50f670c2f4ed1e5e16ad5747be07b9dcce48
kgomez <kgomez@DESKTOP-SQRSML0>
2024-11-23 03:05:06 -0500

    implementacion de los cruds

--- obj/Debug/net8.0/PropertyManagerWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/PropertyManagerWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d1e68d221f2801d79b27e72d596cd6a10c65bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9af2054efaba513b75cf4432ad8218e28b2b6f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PropertyManagerWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
